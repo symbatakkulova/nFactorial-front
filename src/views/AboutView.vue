1f71df3922cf3b51f7c607cb26f75a57126537cc
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2024-04-11 23:54:34 +0500

    front uploading

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,42 @@
+<script>
+import axios from "axios";
+const host = process.env.VUE_APP_SERVER_HOST;
+const port = process.env.VUE_APP_SERVER_PORT;
+
+export default {
+  props:["id"],
+  data(){
+    return{
+      info:""
+    }
+  },
+  methods:{
+    async getInfo(){
+      const url = `http://${host}:${port}/${this.id}`;
+      console.log(url);
+      try {
+        const result = await axios.get(url);
+        const a = result.data.find(e=>e.id===this.id*1);
+        console.log(result);
+        console.log(this.id)
+        this.info = result.data;
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  },
+  mounted(){
+    this.getInfo()
+  }
+}
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="container">
+      <div class="title">
+        <h1>{{info.title}}</h1>
+      </div>
+    </div>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,93 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
+const host = process.env.VUE_APP_SERVER_HOST;
+const port = process.env.VUE_APP_SERVER_PORT;
+const url = `http://${host}:${port}/`
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      list: []
+    }
+
   },
-};
+  methods: {
+    async getList() {
+      try {
+        const result = await axios.get(url);
+        console.log(result);
+        this.list = result.data;
+      } catch (err) {
+        console.log(err)
+      }
+
+    }
+
+
+  },
+  mounted() {
+
+    this.getList()
+  }
+}
 </script>
+<template>
+  <main>
+    <div class="title">
+      <h1>Tengri News</h1>
+    </div>
+    <div class="container">
+      <div class="news">
+        <div v-for="i in list" :key="i.id" class="new">
+          <router-link :to="{ name: 'about', params: { id: i.id } }">
+            <div class="new-title">{{ i.title }}</div>
+            <div class="image">
+              <img :src="i.img" alt="">
+            </div>
+            <div class="date">
+              {{ i.date }}
+            </div>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<style lang="css" scoped>
+* {
+  margin: 0;
+}
+
+.title {
+  margin: 0;
+  padding: 1rem;
+  background-color: green;
+  color: white;
+  text-align: center;
+}
+
+.container {
+  width: 100%;
+  height: 100%;
+}
+
+.news {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.new {
+  width: 20%;
+  text-align: center;
+  margin: 1rem;
+  padding: 1rem;
+  border: 1px solid black;
+
+}
+
+.image img {
+  width: 100%;
+  height: 20%;
+}</style>
